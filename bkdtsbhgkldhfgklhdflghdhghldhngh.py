c40d82bb9de6e34449a3b9f816de375c5e323d5d
sosiska256 <114388129+sosiska256@users.noreply.github.com>
2022-11-07 16:10:30 +0700

    Add files via upload

---
+++ bkdtsbhgkldhfgklhdflghdhghldhngh.py
@@ -0,0 +1,10 @@
+for x in range(0,1000):
+   for a in range(0,1000):
+        if ((x%2==0)<=(not(x%3==0)))or((x+a)>=100):
+            n=True
+        else:
+            n=False
+if n==True:
+    print(a)
+
+    
---
+++ ege16.py
@@ -0,0 +1,7 @@
+a=b=1
+for x in range(1,2024):
+    a=a/x
+for y in range(1,2021):
+    b=b/y
+c=a/b
+print(c)
