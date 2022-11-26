9e8d0a2b6a82c9458e634d5f635a8a84cdf48eb5
sosiska256 <114388129+sosiska256@users.noreply.github.com>
2022-11-26 21:21:43 +0700

    Delete слайд шоу.py

--- слайд шоу.py
+++
@@ -1,9 +0,0 @@
-from moviepy.editor import *
-import os
-directory = 'C:/Users/student/OneDrive/Документы'
-files = os.listdir(directory)
-images = list(filter(lambda x: x.endwith('.jpg'), files))
-clips = [ImageClip(m).set_duration(2) for m in images]
-
-final_clip = concatenate_videoclips([clips], method='compose')
-final_clip.write_videofile('test.mp4', fps=24)
