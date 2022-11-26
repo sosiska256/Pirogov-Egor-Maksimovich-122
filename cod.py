3d807c2333060c734fd8513e9159722e0d4caf32
sosiska256 <114388129+sosiska256@users.noreply.github.com>
2022-11-26 09:53:36 +0700

    Add files via upload

--- cod.py
+++ cod.py
@@ -1,30 +1,11 @@
-d=str(input('vvedite primer'))
-g=d.split()
-a=float(g[0])
-n=g[1]
-b=float(g[2])
-def plus(a,b):
-    c=a+b
-    return c
-def minus(a,b):
-    c=a-b
-    return c
-def multiplication(a,b):
-    c=a*b
-    return c
-def division(a,b):
-    c=a/b
-    return c
-def degree(a,b):
-    c=a**b
-    return c
-if n=='+':
-    print(plus(a,b))
-if n=='-':
-    print(minus(a,b))
-if n=='*':
-    print(multiplication(a,b))
-if n=='/':
-    print(division(a,b))
-if n=='**':
-    print(degree(a,b))
+from moviepy.editor import *
+
+import os
+
+directory = 'C:/Users/student/OneDrive/gog/niga'
+files = os.listdir(directory)
+images = list(filter(lambda x: x.endswith('.jpg'), files))
+clips = [ImageClip(m).set_duration(2) for m in images]
+
+final_clip = concatenate_videoclips(clips, method='compose')
+final_clip.write_videofile('test.mp4', fps=24)
