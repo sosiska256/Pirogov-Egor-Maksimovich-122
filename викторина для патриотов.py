387a86d98da465d1521ad814094a6eeebaa78153
sosiska256 <114388129+sosiska256@users.noreply.github.com>
2022-12-02 21:40:09 +0700

    Add files via upload

---
+++ викторина для патриотов.py
@@ -0,0 +1,24 @@
+a=['Вы поддерживаете КПСС?(1-да,2-нет)','2+2=?','если любое число умножить на 0, то получится?','Небо голубое?(1-да,2-нет)','бип боп бип?(1-да,2-нет)']
+b=int(0)
+for i in range(len(a)):
+    if i==0:
+        c=int(input(f'{a[i]}\n'))
+        if c==1:
+            b=b+1
+    elif i==1:
+        c = int(input(f'{a[i]}\n'))
+        if c==4:
+            b=b+1
+    elif i==2:
+        c = int(input(f'{a[i]}\n'))
+        if c==0:
+            b=b+1
+    elif i==3:
+        c = int(input(f'{a[i]}\n'))
+        if c==1:
+            b=b+1
+    elif i==4:
+        c = int(input(f'{a[i]}\n'))
+        if c==1:
+            b=b+1
+print(b)
