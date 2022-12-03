5741021d19cec37d3198feb9e1894ac300267571
sosiska256 <114388129+sosiska256@users.noreply.github.com>
2022-12-03 09:36:05 +0700

    Update викторина для патриотов.py

--- викторина для патриотов.py
+++ викторина для патриотов.py
@@ -1,24 +1,28 @@
 a=['Вы поддерживаете КПСС?(1-да,2-нет)','2+2=?','если любое число умножить на 0, то получится?','Небо голубое?(1-да,2-нет)','Владимир Путин молодец?(1-да,2-нет)']
 b=int(0)
-for i in range(len(a)):
-    if i==0:
-        c=int(input(f'{a[i]}\n'))
-        if c==1:
-            b=b+1
-    elif i==1:
-        c = int(input(f'{a[i]}\n'))
-        if c==4:
-            b=b+1
-    elif i==2:
-        c = int(input(f'{a[i]}\n'))
-        if c==0:
-            b=b+1
-    elif i==3:
-        c = int(input(f'{a[i]}\n'))
-        if c==1:
-            b=b+1
-    elif i==4:
-        c = int(input(f'{a[i]}\n'))
-        if c==1:
-            b=b+1
+def zxc():
+    for i in range(len(a)):
+        if i==0:
+            c=int(input(f'{a[i]}\n'))
+            if c==1:
+                b=b+1
+        elif i==1:
+            c = int(input(f'{a[i]}\n'))
+            if c==4:
+                b=b+1
+        elif i==2:
+            c = int(input(f'{a[i]}\n'))
+            if c==0:
+                b=b+1
+        elif i==3:
+            c = int(input(f'{a[i]}\n'))
+            if c==1:
+                b=b+1
+        elif i==4:
+            c = int(input(f'{a[i]}\n'))
+            if c==1:
+                b=b+1
 print(b)
+n=int(input('Хотите сыграть ещё раз?'))
+if n==1:
+    zxc()
