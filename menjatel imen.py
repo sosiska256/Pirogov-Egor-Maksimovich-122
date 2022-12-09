563752bc79931633e78d8fc82371f11c025d991d
sosiska256 <114388129+sosiska256@users.noreply.github.com>
2022-12-09 11:13:18 +0700

    Add files via upload

---
+++ menjatel imen.py
@@ -0,0 +1,23 @@
+import random
+a=input('Введи имя')
+pol=int(input('Вы мужчина?(1-да,2-нет)'))
+if pol==1:
+    b=list(a)
+    c=b[2:]
+    var=b[3:]
+    var2=''.join(var)
+    d=''.join(c)
+    v=random.choice(['нер','ор'])
+    z=random.choice(['нер','ор','кс'])
+    print(f'Мар{d}{v}')
+    print(f'Мар{var2}{z}. Какой молодец, а может теперь займешься чем-то полезным?')
+elif pol==2:
+    b=list(a)
+    c=b[2:]
+    var=b[3:]
+    var2=''.join(var)
+    d=''.join(c)
+    v=random.choice(['лина','на','ка'])
+    z=random.choice(['лина','на','ка'])
+    print(f'Мар{d}{v}')
+    print(f'Мар{var2}{z}. Какая умница, а может теперь займешься чем-то полезным?')
