4d641c36d527b0efb2876ec832ca30ef8dab0f1e
sosiska256 <114388129+sosiska256@users.noreply.github.com>
2022-12-10 09:52:43 +0700

    Add files via upload

---
+++ menjatel imen.py
@@ -0,0 +1,37 @@
+import random, os
+def zxc():
+    x=int(input('Нужна ли справка об авторе?(1-да,2-нет): '))
+    if x==1:
+        print(f' Всё сделал-Пирогов Егор')
+    elif x==2:
+        a=input('Введи имя: ')
+        pol=int(input('Вы мужчина?(1-да,2-нет): '))
+        if pol==1:
+            b=list(a)
+            c=b[2:]
+            var=b[3:]
+            var2=''.join(var)
+            d=''.join(c)
+            v=random.choice(['Мар','Муд','Зиг'])
+            z=random.choice(['нер','ор','кс'])
+            k=random.choice(['Мар','Муд','Зиг'])
+            l=random.choice(['нер','ор','кс'])
+            print(f'Ваше имя: {v}{d}{z}')
+            print(f'Альтернативный вариант: {k}{var2}{l}')
+        elif pol==2:
+            b=list(a)
+            c=b[2:]
+            var=b[3:]
+            var2=''.join(var)
+            d=''.join(c)
+            v=random.choice(['Мар','Муд','Зиг'])
+            z=random.choice(['лина','на','ла'])
+            k=random.choice(['Мар','Муд','Зиг'])
+            l=random.choice(['нер','ор','кс'])
+            print(f'Ваше имя: {v}{d}{z}')
+            print(f'Альтернативный вариант: {k}{var2}{l}')
+        m=int(input('Хотите ещё раз запустить?(1-да 2-нет)'))
+        if m==1:
+            os.system('cls')
+            return zxc()
+zxc()
