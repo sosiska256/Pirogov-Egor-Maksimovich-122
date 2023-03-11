bff00cf86e8c45fdb62bcd6235148c464627eaf5
sosiska256 <114388129+sosiska256@users.noreply.github.com>
2023-03-11 13:54:42 +0700

    Update 12.py

--- 2пол/12.py
+++ 2пол/12.py
@@ -1,6 +1,6 @@
 a=[]
 for num in range(2,1000):
-    if all(num%delit!=0 for delit in range(2,num)):
+    if all(num%b!=0 for b in range(2,num)):
         a.append(num)
         
 for y in range (100):
