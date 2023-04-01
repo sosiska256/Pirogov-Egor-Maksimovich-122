603129a2b1b244bf257a3baaf2970f928e9145d5
sosiska256 <114388129+sosiska256@users.noreply.github.com>
2023-04-01 13:37:23 +0700

    Update Списки.py

--- 2пол/Списки.py
+++ 2пол/Списки.py
@@ -1,30 +1,16 @@
-import random
-def sort(isreverse):
-    a = [int(random.random() * 100) for _ in range(int(input('количество элементов списка: ')))]
-    for j in range(len(a)):
-        maxa = 0
-        for i in range(len(a) - j):
-            if a[i] > maxa:
-                maxa = a[i]
-        a.append(a.pop(a.index(maxa)))
-        print(a)
-    if isreverse:
-        a = a[::1]
-    else:
-        a = a[::-1]
-    return a
-def binary():
-    a = [25, 6, 4, 18, 32, 64, 124]
+a=[int(x) for x in input().split()]
+e=len(a)
+Fl=False
+while not(Fl):
+    for i in range(e):
+        f=a[i]
+        for x in range(i,e):
+            if f<=a[x]:
+                continue
+            else:
+                a[i],a[x]=a[x],a[i]
+        
     print(a)
-    while len(a) != 1:
-        if a[len(a) // 2] > a[len(a) // 2 - 1]:
-            a = a[:len(a) // 2:]
-        else:
-            a = a[len(a) // 2::]
-    print(a[0])
-if __name__ == '__main__':
-    i = int(input('1 - сортировка рандомного списка\n 2 - бинарный поиск'))
-    if i == 1:
-        print(sort(True))
-    elif i == 2:
-        binary()
+    if all(a[i]<=a[i+1] for i in range(e-1)):
+        Fl=True
+print('end - ',a)
