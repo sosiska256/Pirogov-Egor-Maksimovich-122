d4c2065f962b86de870a63e05df64a36194e61f6
sosiska256 <114388129+sosiska256@users.noreply.github.com>
2023-04-01 13:55:12 +0700

    Update Списки.py

--- 2пол/Списки.py
+++ 2пол/Списки.py
@@ -1,16 +1,15 @@
-a=[int(x) for x in input().split()]
+a=[int(x) for x in input().split()]  # список
 e=len(a)
 Fl=False
-while not(Fl):
-    for i in range(e):
+while not(Fl):      # бесконечный цикл
+    for i in range(e):          # сравнение элемента со всеми элементами списка
         f=a[i]
         for x in range(i,e):
             if f<=a[x]:
                 continue
             else:
                 a[i],a[x]=a[x],a[i]
-        
-    print(a)
-    if all(a[i]<=a[i+1] for i in range(e-1)):
+    if all(a[i]<=a[i+1] for i in range(e-1)):   # проверка на верную сортировку
         Fl=True
-print('end - ',a)
+print('end - ',a)   
+
